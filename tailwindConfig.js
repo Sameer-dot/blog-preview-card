8581d2e2f07f59a9e9844ee22436a2a3e7c7f359
Muhammad Sameer Naveed <68469246+Sameer-dot@users.noreply.github.com>
2024-03-07 16:52:15 +0500

    Blog Preview Card Created

---
+++ tailwindConfig.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        yellow: "hsl(47, 88%, 63%)",
+        white: "hsl(0, 0%, 100%)",
+        grey: "hsl(0, 0%, 50%)",
+        black: "hsl(0, 0%, 7%)",
+      },
+    },
+  },
+};
